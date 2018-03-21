bf3e7e3d14845389685e6b087ca6eca6fb8f1497
Amanda <albullington@gmail.com>
2018-03-21 11:24:32 -0700

    First two parts working.

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-// TODO
+// var GroceryList = () => (
+// 	<ul>
+// 	  <li>Kale</li>
+// 	  <li>Cucumbers</li>
+// 	</ul>
+// );
+
+var GroceryList = () => (
+	<ul>
+    <Kale />
+    <Cucumbers />
+  </ul>
+);
+
+var Kale = () => (
+    <li>kale</li>
+);
+
+var Cucumbers = () => (
+    <li>cucumbers</li>
+);
+
+  	ReactDOM.render(<GroceryList />, document.getElementById('app'));
+  	ReactDOM.render(<Kale />, document.getElementById('ul'));
+  	ReactDOM.render(<Cucumbers />, document.getElementById('ul'));
+  	
